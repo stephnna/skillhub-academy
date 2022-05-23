fda7ccf9ec6d37f4b25d3dd9299964ba10a3a9ff
stephnna <stephennnabuike@gmail.com>
2022-05-23 11:57:49 +0100

    implemet script

--- script.js
+++ script.js
@@ -1,8 +1,64 @@
-function myAdd(a, b) {
-  let c = 0;
-  c = a + b;
+// creating first card item i.e grid project
 
-  return c;
+const projects = [{ 
+  image: './images/chinedu-ezea-picture.png',
+  name: 'Chinedu Ezea',
+  dash: '',
+  qualification: 'Chinedu is a doctor of philosophy at Nnamdi Azikiwe University',
+  description: 'He is a mathematician and a professional editors with several publications in high impact factor journals', 
+},
+{
+  image: './images/chinedu-ezea-picture.png',
+  name: 'Chinedu Ezea',
+  dash: '',
+  qualification: 'Chinedu is a doctor of philosophy at Nnamdi Azikiwe University',
+  description: 'He is a mathematician and a professional editors with several publications in high impact factor journals', 
+},
+{ 
+  image: './images/chinedu-ezea-picture.png',
+  name: 'Chinedu Ezea',
+  dash: '',
+  qualification: 'Chinedu is a doctor of philosophy at Nnamdi Azikiwe University',
+  description: 'He is a mathematician and a professional editors with several publications in high impact factor journals', 
+},
+{
+  image: './images/chinedu-ezea-picture.png',
+  name: 'Chinedu Ezea',
+  dash: '',
+  qualification: 'Chinedu is a doctor of philosophy at Nnamdi Azikiwe University',
+  description: 'He is a mathematician and a professional editors with several publications in high impact factor journals', 
+},
+{ 
+  image: './images/chinedu-ezea-picture.png',
+  name: 'Chinedu Ezea',
+  dash: '',
+  qualification: 'Chinedu is a doctor of philosophy at Nnamdi Azikiwe University',
+  description: 'He is a mathematician and a professional editors with several publications in high impact factor journals', 
+},
+{ 
+  image: './images/chinedu-ezea-picture.png',
+  name: 'Chinedu Ezea',
+  dash: '',
+  qualification: 'Chinedu is a doctor of philosophy at Nnamdi Azikiwe University',
+  description: 'He is a mathematician and a professional editors with several publications in high impact factor journals', 
+},
+];
+
+const sectionFeaturedElement = document.getElementById('featured-speakers-id');
+sectionFeaturedElement.className = 'featured-speakers';
+
+for (let i = 0; i < projects.length; i += 1) {
+  const gridProject = document.createElement('div');
+  gridProject.className = 'flex-container featured-grid-item display-flex';
+  sectionFeaturedElement.appendChild(gridProject);
 }
 
-myAdd(3, 5);
+Array.from(sectionFeaturedElement.children).forEach((child, index) => {
+  child.innerHTML = `  
+  <div class="chinedu-image" style="background-image: url(${projects[index].image});"></div>
+  <div><h4>${projects[index].name}</h4>
+  <p>${projects[index].qualification}</p>
+  <p>${projects[index].description}</p>
+</div>    
+`;
+});
